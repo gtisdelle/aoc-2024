a0cbd7df5b3f75b346c0709ad1f75b8496efebe7
George Tisdelle <tisdellegeorge@gmail.com>
2024-12-07 09:33:02 -0500

    refactor day 2 to iterators

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,31 +5,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filepath = &args[1];
 
-    let mut safe = 0;
-    for report in read_to_string(filepath).unwrap().lines() {
-        let levels: Vec<i32> = report
-            .split(' ')
-            .map(|level| level.parse().unwrap())
-            .collect();
+    let num_safe = read_to_string(filepath)
+        .expect("File should be read")
+        .lines()
+        .map(|report| report.split(' ').map(|level| level.parse::<i32>().unwrap()))
+        .filter(|report| is_report_safe(report.clone().collect()))
+        .count();
 
-        if levels[1] == levels[0] || (levels[1] - levels[0]).abs() > 3 {
-            continue;
-        }
+    dbg!(num_safe);
+}
 
-        let asc = levels[1] > levels[0];
-        let mut is_record_safe = true;
-        for i in 1..levels.len() - 1 {
-            let diff = levels[i + 1] - levels[i];
-            if (asc && diff <= 0) || (!asc && diff >= 0) || diff.abs() > 3 {
-                is_record_safe = false;
-                break;
-            }
-        }
+fn is_report_safe(report: Vec<i32>) -> bool {
+    if report[1] == report[0] || (report[1] - report[0]).abs() > 3 {
+        return false;
+    }
 
-        if is_record_safe {
-            safe += 1;
+    let asc = report[1] > report[0];
+    for i in 1..report.len() - 1 {
+        let diff = report[i + 1] - report[i];
+        if (asc && diff <= 0) || (!asc && diff >= 0) || diff.abs() > 3 {
+            return false;
         }
     }
 
-    dbg!(safe);
+    true
 }
